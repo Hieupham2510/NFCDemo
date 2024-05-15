a480d041926057502a796c50baf0f4393db8ff11
hieupt3 <hieupt3@pvcombank.com.vn>
2024-05-15 16:08:59 +0700

    Initial Commit

---
+++ NFCDemo/NFCDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCDemoApp.swift
+//  NFCDemo
+//
+//  Created by Pham Trung Hieu on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NFCDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
